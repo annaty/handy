0a4aab8a9f66065b72c41c4f291622932a1ceee0
annaty <tylkowskanna@gmail.com>
2021-03-14 23:27:57 +0100

    small config change

--- config/config.php
+++ config/config.php
@@ -5,7 +5,6 @@ return [
         'name' => 'test',
         'username' => 'root',
         'password' => '',
-        'connection' => 'mysql:host=127.0.0.1',
         'options' => [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING
         ]
